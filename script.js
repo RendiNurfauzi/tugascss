6672394dd00f50ab7dd8f53d9fc7855bfe2c5ed1
RendiNurfauzi <85463301+RendiNurfauzi@users.noreply.github.com>
2024-04-14 21:55:10 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// JavaScript
-document.getElementById('hamburgerCheckbox').addEventListener('change', function() {
-    document.getElementById('menu').style.transform = this.checked ? 'translate(0, 0)' : 'translate(-100%, 0)';
-  });
-  
